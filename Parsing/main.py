81b8bbb338a966d073a7311657e94c915f522ed3
MagGoldi <stivenH0king@yandex.ru>
2023-10-31 20:26:20 +0400

    add working parser + testing

--- Parsing/main.py
+++ Parsing/main.py
@@ -1,4 +1,46 @@
-from parsing import run
+import os
 
-if __name__ == '__main__':
-    run()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+
+driver = webdriver.Chrome()
+
+
+def get_location(url_proxies):
+    driver.get(url_proxies)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    ip = soup.find("div", class_="ip").text.strip()
+    location = soup.find("div", class_="value-country").text.strip()
+
+    print(f"IP: {ip}\nLocation: {location}")
+
+
+def parsing(url):
+    driver.get(url)
+    # print(driver.page_source)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    meta_tags = soup.find_all("meta", {"itemprop": "price"})
+    price_values = [meta["content"] for meta in meta_tags]
+
+    for price in price_values:
+        print(price)
+
+
+def main(path_to_csv=os.path.join("PYTHON", "dataset.csv")):
+    file_name = open("dataset.csv", "w", encoding="utf-8")
+    url = "https://www.avito.ru/samara/avtomobili/s_probegom/audi-ASgBAQICAUSGFMjmAQFA4LYNFN6XKA?cd=1&f=ASgBAQICAkSGFMjmAfrwD~i79wIBQOC2DRTelyg&radius=200&searchRadius=200"
+    url_proxies = "https://2ip.ru"
+
+    get_location(url_proxies)
+    parsing(url)
+
+    driver.quit()
+    file_name.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Parsing/test.py
@@ -0,0 +1,4 @@
+data_avto = []
+
+file_name = open("dataset.csv", "w", encoding="utf-8")
+file_name.write()
