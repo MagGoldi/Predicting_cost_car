4ac2336aa5ab6b6f4c64bcca048d358a1f76a444
studypankeev@gmail.com <stivenH0king@yandex.ru>
2023-12-11 23:50:41 +0400

    work with data

--- Parsing/local_test.py
+++ Parsing/local_test.py
@@ -1,8 +1,10 @@
-import csv
-import datetime
+import pandas as pd
+import re
 
-# Создаем название файла с текущей датой
-filename = "car_data_" + str(datetime.date.today())
-print(filename)
-# Создаем файл и записываем в него данные
-# with open(filename, mode='w', newline='') as file:
+data = {
+    "Engine_capacity": ["3.0 AT (286 л.с.)", "2.5 MT (180 л.с.)", "4.4 AT (450 л.с.)"]
+}
+df = pd.DataFrame(data)
+df["Engine_capacity"] = df["Engine_capacity"].apply(lambda x: re.findall(r"\d+", x)[-1])
+
+print(df)
